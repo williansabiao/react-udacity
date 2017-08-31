56be2c8942fa526513c3dc9ff5e7f8f2f8d76ea7
Willian R. Sabião <willianjedai@gmail.com>
2017-08-31 23:37:36 +0100

    PropTypes lesson

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 function ListContacts (props) {
   return (
@@ -20,5 +21,10 @@ function ListContacts (props) {
     </ol>
   )
 }
+
+ListContacts.propTypes = {
+  contacts: propTypes.array.isRequired,
+  onDeleteContact: propTypes.func.isRequired
+};
 
 export default ListContacts;
