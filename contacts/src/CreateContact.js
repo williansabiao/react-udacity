daaebf835d71361a9fff2ef5e63bda6e6d741241
Willian R. Sabião <willianjedai@gmail.com>
2017-09-19 21:09:28 +0100

    starting react route

---
+++ contacts/src/CreateContact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CreateContact extends Component {
+  render() {
+    return (
+      <div>Create contact!</div>
+
+    )
+  };
+}
+
+export default CreateContact;
